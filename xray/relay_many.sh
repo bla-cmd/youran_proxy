9b3cb0fb87a8ce6b6accbe7d0ea263bf42618786
youran <a446150793@gmail.com>
2024-10-04 15:19:57 +0800

    f

--- xray/relay_many.sh
+++ xray/relay_many.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# 提示用户输入IP地址
+read -p "请输入要转发的IP地址: " ip_address
+
+# /usr/local/etc/xray/config.json 的目标路径
+config_path="/usr/local/etc/xray/config.json"
+
+# 检查文件是否存在，如果存在则删除
+if [ -f "$config_path" ]; then
+    rm -f "$config_path"
+    echo "已删除现有的 $config_path 文件。"
+fi
+
+# 创建新的 config.json 内容
+config_content=$(cat <<EOF
 {
   "log": null,
   "routing": {
@@ -421,3 +437,26 @@
   "reverse": null,
   "fakeDns": null
 }
+
+EOF
+)
+
+
+# 确保 /usr/local/etc/xray/ 目录存在
+mkdir -p /usr/local/etc/xray/
+
+# 创建新的 config.json 文件
+echo "$config_content" > "$config_path"
+echo "default配置已创建并保存到 $config_path"
+
+
+# 重新启动 xray 服务
+killall xray
+systemctl restart xray
+
+# 检查服务状态
+if [ $? -eq 0 ]; then
+    echo "xray 服务已成功重启。"
+else
+    echo "xray 服务重启失败，请检查配置文件和服务状态。"
+fi
