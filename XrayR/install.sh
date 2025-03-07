68f19e06d7315b1288427a27cd727e4303473de6
libosen <m@yonrd.com>
2025-03-07 19:39:21 +0800

    f

--- XrayR/install.sh
+++ XrayR/install.sh
@@ -109,7 +109,8 @@ install_XrayR() {
 	cd /usr/local/XrayR/
 
     if  [ $# == 0 ] ;then
-        last_version=$(curl -Ls "https://github.moeyy.xyz/https://api.github.com/repos/XrayR-project/XrayR/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+#        last_version=$(curl -Ls "https://github.moeyy.xyz/https://api.github.com/repos/XrayR-project/XrayR/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version="v0.9.4"
         if [[ ! -n "$last_version" ]]; then
             echo -e "${red}检测 XrayR 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 XrayR 版本安装${plain}"
             exit 1
